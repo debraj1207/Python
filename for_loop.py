08a4b2599d08f7b8b76d5cd00dda17a06b5e69ae
Debraj <65642893+debraj1207@users.noreply.github.com>
2020-05-24 19:41:17 +0530

    basics of loop-statements

---
+++ for_loop.py
@@ -0,0 +1,19 @@
+
+#Example: Python for Loop
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+# Program to find the sum of all numbers stored in a list
+
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+sum = 0
+# iterate over the list
+for val in numbers:
+	sum = sum+val
+
+print("The sum is", sum)
+	
---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#example of while loop:
+
+
+#sum of the numbers:
+
+sum=0
+n= int(input())
+i=0
+while(i<=n):
+    sum += i
+    i += 1
+print(sum)
